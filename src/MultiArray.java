9ef72785fec062044b358e4c507b1051f5611489
miki1989 <mikolajheller@gmail.com>
2018-07-15 18:31:24 +0200

    First part.

---
+++ src/MultiArray.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class MultiArray {
+
+    Random rand = new Random();
+
+    public int [][] tablica;
+
+    public MultiArray(int n, int m){
+        n = rand.nextInt(10);
+        m = rand.nextInt(10);
+        this.tablica[n] = tablica[n];
+        this.tablica[m] = tablica[m];
+        }
+
+    public int[][] getTablica() {
+        return tablica;
+    }
+
+    public void setTablica(int[][] tablica) {
+        this.tablica = tablica;
+    }
+
+    public void randomize(){
+
+    }
+
+    public void findMin(){
+
+    }
+
+    public void findMax(){
+
+    }
+
+    public void info(){
+
+    }
+
+}
---
+++ src/MultiTest.java
@@ -0,0 +1,5 @@
+public class MultiTest {
+    public static void main(String[] args) {
+
+    }
+}
